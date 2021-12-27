c0d102979360aae7d763c1ab24a28f2a854be0e0
Guboneui <starku2249@naver.com>
2021-12-28 02:08:05 +0900

    disposable 까지

--- SwiftWithRxSwift/ViewController.swift
+++ SwiftWithRxSwift/ViewController.swift
@@ -29,7 +29,7 @@ struct LogoutResponse: Decodable {
 }
 
 
-//let MEMBER_LIST_URL = "https://my.api.mockaroo.com/members_with_avatar.json?key=44ce18f0"
+let MEMBER_LIST_URL = "https://my.api.mockaroo.com/members_with_avatar.json?key=44ce18f0"
 
 
 class ViewController: UIViewController {
@@ -38,6 +38,8 @@ class ViewController: UIViewController {
     @IBOutlet weak var editView: UITextView!
     @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
     
+    var disposable = DisposeBag()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true) { [weak self] _ in
@@ -45,6 +47,7 @@ class ViewController: UIViewController {
             self.timerLabel.text = "\(Date().timeIntervalSince1970)"
         }
     }
+    
     
     private func setVisibleWithAnimation(_ v: UIView?, _ s: Bool) {
         guard let v = v else { return }
@@ -56,8 +59,18 @@ class ViewController: UIViewController {
         })
     }
     
-    func downloadJson(_ url: String) -> Observable<LogoutResponse?> {
+    // Observable의 생명주기
+    // 1. create
+    // 2. Subscribe
+    // 3. onNext
+    // 4. onCompleted || onError
+    // 5. Disposed
+    
+    
+    
+    func downloadJson(_ url: String) -> Observable<String?> {
 
+        return Observable.just("hello world")
 //        return Observable.create { emmiter in
 //            let url = URL(string: url)!
 //            let task = URLSession.shared.dataTask(with: url) { (data, _, error) in
@@ -78,54 +91,29 @@ class ViewController: UIViewController {
 //            }
 //        }
         
-        return Observable.create { k -> Disposable in
-            AF.request(url, method: .get, headers: nil)
-                .validate()
-                .responseDecodable(of: LogoutResponse.self) { response in
-                    switch response.result {
-                    case .success(let data):
-
-                        if data.isSuccess {
-                            print("로그아웃 성공")
-                            k.onNext(data)
-                            k.onCompleted()
-                        } else {
-                            print("로그아웃 실패")
-                            k.onNext(data)
-                            k.onCompleted()
-                        }
-                        
-                        
-                        
-                    case .failure(let error):
-                        print(error)
-                        k.onError(error)
-                    }
-
-            }
-            return Disposables.create()
-        }
-
-
+//        return Observable.create { k -> Disposable in
+//            AF.request(url, method: .get, headers: nil)
+//                .validate()
+//                .responseDecodable(of: LogoutResponse.self) { response in
+//                    switch response.result {
+//                    case .success(let data):
+//                        print(data)
+//                        if data.isSuccess {
+//                            print("로그아웃 성공")
+//                            k.onNext(data)
+//                            k.onCompleted()
+//                        } else {
+//                            print("로그아웃 실패")
+//                            k.onNext(data)
+//                            k.onCompleted()
+//                        }
 //
+//                    case .failure(let error):
+//                        print(error)
+//                        k.onError(error)
+//                    }
 //
-        
-        
-        
-        
-        
-//                return Observable.create { f in
-//            DispatchQueue.global().async {
-//                let url = URL(string: url)!
-//                let data = try! Data(contentsOf: url)
-//                let json = String(data: data, encoding: .utf8)
-//                print(json)
-//                DispatchQueue.main.async {
-//                    f.onNext(json)
-//                    f.onCompleted() // onCompleted()를 통해서 순환참조 문제를 해결할 수 있다.
-//                }
 //            }
-//
 //            return Disposables.create()
 //        }
         
@@ -136,23 +124,53 @@ class ViewController: UIViewController {
         editView.text = ""
         
         self.setVisibleWithAnimation(self.activityIndicator, true)
-        downloadJson("http://3.134.232.146:8000/auth/logout")
-            .subscribe { event in
-                // 성공시 next() 로 묶여서 나옴
-                switch event {
-                case .next(let json) :  // next로 넘어오는 데이터를 json 이라고 칭함
-                    print(json)
-                    //self.editView.text = json
-                    self.setVisibleWithAnimation(self.activityIndicator, false)
-                    
-                case .completed:
-                    print("completed")
-                    break
-                case .error(_):
-                    break
-                }
-                
-            }
+        
+        downloadJson(MEMBER_LIST_URL)
+            .observe(on: MainScheduler.instance)
+            .subscribe(on: ConcurrentDispatchQueueScheduler(qos: .default))
+            .subscribe(onNext: { json in
+                self.editView.text = json
+                self.setVisibleWithAnimation(self.activityIndicator, false)
+            }).disposed(by: disposable)
+    
+//            .subscribe { event in   //subscribe가 붙었을 때 옵저버블은 실행된다.
+//                switch event {
+//                case let .next(json):
+////                    DispatchQueue.main.async {
+////                        self.editView.text = json
+////                        self.setVisibleWithAnimation(self.activityIndicator, false)
+////                    }
+//                    print(json)
+//                case .completed:
+//                    break
+//                case .error:
+//                    break
+//                }
+//            }
+
+        
+        
+        
+        
+        
+        
+//        downloadJson("http://3.134.232.146:8000/auth/logout")
+//            .subscribe { event in
+//                // 성공시 next() 로 묶여서 나옴
+//                switch event {
+//                case .next(let json) :  // next로 넘어오는 데이터를 json 이라고 칭함
+//                    print(json)
+//                    //self.editView.text = json
+//                    self.setVisibleWithAnimation(self.activityIndicator, false)
+//
+//                case .completed:
+//                    print("completed")
+//                    break
+//                case .error(_):
+//                    break
+//                }
+//
+//            }
         
         
         // 1. 비동기로 생기는 데이터를 Observable로 감싸서 리턴하는 방법
