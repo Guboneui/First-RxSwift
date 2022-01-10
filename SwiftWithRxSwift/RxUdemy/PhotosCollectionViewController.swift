a56a7218727d0fcf8aa0a1c029d521b8a90a508b
Guboneui <starku2249@naver.com>
2022-01-11 03:48:47 +0900

    camera filter default UI

---
+++ SwiftWithRxSwift/RxUdemy/PhotosCollectionViewController.swift
@@ -0,0 +1,13 @@
+//
+//  PhotosCollectionViewController.swift
+//  SwiftWithRxSwift
+//
+//  Created by 구본의 on 2022/01/11.
+//
+
+import Foundation
+import UIKit
+
+class PhotosCollectionViewController: UICollectionViewController {
+    
+}
---
+++ SwiftWithRxSwift/RxUdemy/PhotosViewController.swift
@@ -0,0 +1,20 @@
+//
+//  PhotosViewController.swift
+//  SwiftWithRxSwift
+//
+//  Created by 구본의 on 2022/01/11.
+//
+
+import UIKit
+
+class PhotosViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.navigationController?.navigationBar.prefersLargeTitles = true
+        
+    }
+    
+
+
+}
