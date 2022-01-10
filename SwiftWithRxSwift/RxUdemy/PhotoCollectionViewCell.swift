714a3b7a3b071b41c74f45695ac85c17345e6caa
Guboneui <starku2249@naver.com>
2022-01-11 04:09:58 +0900

    show photo default

---
+++ SwiftWithRxSwift/RxUdemy/PhotoCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  PhotoCollectionViewCell.swift
+//  SwiftWithRxSwift
+//
+//  Created by 구본의 on 2022/01/11.
+//
+
+import UIKit
+
+class PhotoCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var photo: UIImageView!
+}
--- SwiftWithRxSwift/RxUdemy/PhotosCollectionViewController.swift
+++ SwiftWithRxSwift/RxUdemy/PhotosCollectionViewController.swift
@@ -7,7 +7,49 @@
 
 import Foundation
 import UIKit
+import Photos
 
 class PhotosCollectionViewController: UICollectionViewController {
     
+    private var images = [PHAsset]()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        populatePhotos()
+    }
+    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return self.images.count
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "PhotoCollectionViewCell", for: indexPath) as? PhotoCollectionViewCell else {fatalError()}
+        let asset = self.images[indexPath.row]
+        let manager = PHImageManager.default()
+        manager.requestImage(for: asset, targetSize: CGSize(width: 100, height: 100), contentMode: .aspectFit, options: nil) { image, _ in
+            DispatchQueue.main.async {
+                cell.photo.image = image
+            }
+        }
+        return cell
+    }
+    
+    private func populatePhotos() {
+        PHPhotoLibrary.requestAuthorization { [weak self] status in
+            if status == .authorized {
+                // access the photos from library
+                let assets = PHAsset.fetchAssets(with: PHAssetMediaType.image, options: nil)
+                assets.enumerateObjects { (obejct, count, stop) in
+                    self?.images.append(obejct)
+                }
+                self?.images.reverse()
+                DispatchQueue.main.async {
+                    self?.collectionView.reloadData()
+                }
+                
+            }
+        }
+    }
 }
