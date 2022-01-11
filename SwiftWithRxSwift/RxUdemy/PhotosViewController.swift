0b4de5d119153b63c430da7ceb53f869f38e0299
Guboneui <starku2249@naver.com>
2022-01-12 00:49:53 +0900

    add Image remove protocol delegate

--- SwiftWithRxSwift/RxUdemy/PhotosViewController.swift
+++ SwiftWithRxSwift/RxUdemy/PhotosViewController.swift
@@ -6,15 +6,29 @@
 //
 
 import UIKit
+import RxSwift
 
 class PhotosViewController: UIViewController {
 
+    @IBOutlet weak var photoImageView: UIImageView!
+    
+    let disposeBag = DisposeBag()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationController?.navigationBar.prefersLargeTitles = true
         
     }
     
-
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let navC = segue.destination as? UINavigationController,
+              let photosCVC = navC.viewControllers.first as? PhotosCollectionViewController else {
+                  fatalError("sugue error")
+              }
+        photosCVC.selectedPhoto.subscribe(onNext: { [weak self] photo in
+            guard let self = self else {return}
+            self.photoImageView.image = photo
+        }).disposed(by: disposeBag)
+    }
 
 }
