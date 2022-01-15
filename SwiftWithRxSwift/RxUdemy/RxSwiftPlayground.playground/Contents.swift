2207736097a3aacd017921707a4f8e13b1a8055f
Guboneui <starku2249@naver.com>
2022-01-15 23:29:25 +0900

    combining operators - merge

--- SwiftWithRxSwift/RxUdemy/RxSwiftPlayground.playground/Contents.swift
+++ SwiftWithRxSwift/RxUdemy/RxSwiftPlayground.playground/Contents.swift
@@ -288,9 +288,28 @@ struct Student {
 //}).disposed(by: disposeBag)
 
 
-let first = Observable.of(1, 2, 3)
-let second = Observable.of(4, 5, 6)
-let observable = Observable.concat([first, second])
+// concat
+//let first = Observable.of(1, 2, 3)
+//let second = Observable.of(4, 5, 6)
+//let observable = Observable.concat([first, second])
+//observable.subscribe(onNext: {
+//    print($0)
+//}).disposed(by: disposeBag)
+
+
+// merge - 들어오는 순서에 맞춰서 추가
+
+let left = PublishSubject<Int>()
+let right = PublishSubject<Int>()
+
+let source = Observable.of(left.asObserver(), right.asObserver())
+let observable = source.merge()
 observable.subscribe(onNext: {
     print($0)
 }).disposed(by: disposeBag)
+
+left.onNext(5)
+left.onNext(3)
+right.onNext(2)
+right.onNext(1)
+left.onNext(99)
