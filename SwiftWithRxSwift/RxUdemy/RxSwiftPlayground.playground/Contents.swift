8ff5ae8af986903493bc0e5559ac5d1e1236d230
Guboneui <starku2249@naver.com>
2022-01-15 23:43:00 +0900

    combining operators - scan

--- SwiftWithRxSwift/RxUdemy/RxSwiftPlayground.playground/Contents.swift
+++ SwiftWithRxSwift/RxUdemy/RxSwiftPlayground.playground/Contents.swift
@@ -348,15 +348,21 @@ struct Student {
 //button.onNext("")
 //button.onNext("")
 
-let source = Observable.of(1, 2, 3)
-source.reduce(0, accumulator: +)
+//let source = Observable.of(1, 2, 3)
+//source.reduce(0, accumulator: +)
+//    .subscribe(onNext: {
+//        print($0)
+//    }).disposed(by: disposeBag)
+//
+//source.reduce(0, accumulator: {
+//    summary, newValue in
+//    return summary + newValue
+//}).subscribe(onNext: {
+//    print($0)
+//}).disposed(by: disposeBag)
+
+let source = Observable.of(1, 2, 3, 4, 5, 6)
+source.scan(0, accumulator: -)
     .subscribe(onNext: {
         print($0)
     }).disposed(by: disposeBag)
-
-source.reduce(0, accumulator: {
-    summary, newValue in
-    return summary + newValue
-}).subscribe(onNext: {
-    print($0)
-}).disposed(by: disposeBag)
