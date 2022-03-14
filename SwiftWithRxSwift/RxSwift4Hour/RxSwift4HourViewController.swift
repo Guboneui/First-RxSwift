46e3a0ffd37f3d0529a75e797cf6e4a54a60fe0f
Guboneui <starku2249@naver.com>
2022-03-15 05:39:13 +0900

    Base Commit

---
+++ SwiftWithRxSwift/RxSwift4Hour/RxSwift4HourViewController.swift
@@ -0,0 +1,26 @@
+//
+//  RxSwift4HourViewController.swift
+//  SwiftWithRxSwift
+//
+//  Created by 구본의 on 2022/03/15.
+//
+
+import UIKit
+import RxSwift
+
+// RxSwift 공식 홈페이지의 Docs를 보면 5가지가 나오게 된다.
+// 5가지는 Observable, Operators, Single, Subject, Scheduler 로 구성되어 있으며
+// 사실상 가장 중요한 부분은 Observable, Operators, Scheduler 순으로 생각할 수 있다.
+// 각각의 역할들은 다시 정리할 예정이며,
+// 자세한 내용은 해당 링크에서 확인할 수 있다. -> https://reactivex.io/documentation/ko/observable.html
+
+class RxSwift4HourViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
