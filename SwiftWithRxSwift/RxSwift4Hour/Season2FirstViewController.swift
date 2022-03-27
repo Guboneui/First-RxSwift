2d47fb746dfee0434af5eb9708f4312de060994c
Guboneui <starku2249@naver.com>
2022-03-28 04:33:23 +0900

    escaping 함수의 파라미터로 클로저가 들어갔을 경우 본체 함수가 실행되고 나서도 실행되는 함수임을 명시하기 위해 @escaping을 붙여준다.

--- SwiftWithRxSwift/RxSwift4Hour/Season2FirstViewController.swift
+++ SwiftWithRxSwift/RxSwift4Hour/Season2FirstViewController.swift
@@ -9,10 +9,10 @@ import UIKit
 import RxSwift
 import SwiftyJSON
 
-let IMAGE_URL = "https://my.api.mockaroo.com/members_with_avatar.json?key=44ce18f0"
+let TEST_URL = "https://my.api.mockaroo.com/members_with_avatar.json?key=44ce18f0"
 
 class Season2FirstViewController: UIViewController {
-
+    
     @IBOutlet weak var timerLabel: UILabel!
     @IBOutlet weak var editView: UITextView!
     @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
@@ -23,7 +23,7 @@ class Season2FirstViewController: UIViewController {
         Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true) { [weak self] _ in
             self?.timerLabel.text = "\(Date().timeIntervalSince1970)"
         }
-
+        
     }
     
     private func setVisibleWithAnimation(_ v: UIView?, _ s: Bool) {
@@ -34,19 +34,27 @@ class Season2FirstViewController: UIViewController {
             self?.view.layoutIfNeeded()
         })
     }
-
-  
-    @IBAction func onLoad(_ sender: Any) {
-        editView.text = ""
-        setVisibleWithAnimation(self.activityIndicator, true)
-        DispatchQueue.global().async {[self] in
-            let url = URL(string: IMAGE_URL)!
+    
+    func downloadJson(_ url: String, _ completion: @escaping (String?) -> Void) {
+        DispatchQueue.global().async {
+            let url = URL(string: url)!
             let data = try! Data(contentsOf: url)
             let json = String(data: data, encoding: .utf8)
             DispatchQueue.main.async {
-                self.editView.text = json
-                self.setVisibleWithAnimation(self.activityIndicator, false)
+                completion(json)
             }
         }
     }
+    
+    
+    @IBAction func onLoad(_ sender: Any) {
+        editView.text = ""
+        setVisibleWithAnimation(self.activityIndicator, true)
+        
+        self.downloadJson(TEST_URL) { json in
+            self.editView.text = json
+            self.setVisibleWithAnimation(self.activityIndicator, false)
+        }
+    }
 }
+
