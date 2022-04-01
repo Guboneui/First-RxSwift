100be608dae11b7624dc17c40073ed2cc843b8f9
Guboneui <starku2249@naver.com>
2022-04-02 01:34:06 +0900

    View에 보여지게 되는 ViewModel 생성 init 생성자를 통해 해당 뷰모델이 생성되는 시점에 정의

--- SwiftWithRxSwift/RxSwift4Hour/MVVM/Pages/MenuList/MenuListViewModel.swift
+++ SwiftWithRxSwift/RxSwift4Hour/MVVM/Pages/MenuList/MenuListViewModel.swift
@@ -11,6 +11,15 @@ import RxRelay
 
 // 모든 데이터에 대한 처리는 ViewModel에서 처리한다.
 
+
+protocol MenuViewModelType {
+    var fetchMenus: AnyObserver<Void> { get }
+    var clearSelectoins: AnyObserver<Void> { get }
+    var makeOrder: AnyObserver<Void> { get }
+    //var increaseMenuCount: AnyObserver<(menu: View)
+    
+}
+
 class MenuListViewModel {
 
     // UI와 연결된 서브젝트는 에러가 난다고 해서 스트림이 끊어지면 안됨 -> UI에서도 끊어지기 때문에
@@ -20,87 +29,87 @@ class MenuListViewModel {
     //var menuObservable = BehaviorSubject<[Menu]>(value: [])
     // subject에서는 next, error, completed 를 뱉어내지만
     // relay에서는 내보내기만 하기 때문에 onNext가 아닌 accept를 사용한다.
-    
-    var menuObservable = BehaviorRelay<[Menu]>(value: [])
-    
-    var disposeBag = DisposeBag()
-    //var itemsCount: Int = 5
-    //var totalPrice: PublishSubject<Int> = PublishSubject()
-    
-    lazy var itemCount = menuObservable.map {
-        $0.map { $0.count }.reduce(0, +)
-    }
-    
-    lazy var totalPrice = menuObservable.map {
-        $0.map{ $0.price * $0.count }.reduce(0, +)
-    }
-    
-    // subject
-    // 서브젝트는 옵저버블 처럼 구독해서 값을 받을수도 있지만, 외부에서 값을 통제할 수 도 있다.
-
-    init() {
-//        var menus: [Menu] = [
-//            Menu(id: 0, name: "튀김1", price: 100, count: 0),
-//            Menu(id: 1, name: "튀김2", price: 100, count: 0),
-//            Menu(id: 2, name: "튀김3", price: 100, count: 0),
-//        ]
-        
-        _ = APIService.fetchAllMenusRx()
-            .map { data -> [MenuItem] in
-                struct Response: Decodable {
-                    let menus: [MenuItem]
-                }
-                let response = try! JSONDecoder().decode(Response.self, from: data)
-                return response.menus
-            }.map{ menuItems -> [Menu] in
-                var menus: [Menu] = []
-                menuItems.enumerated().forEach { (index, item) in
-                    let menu = Menu.fromMenuItems(id: index, item: item)
-                    menus.append(menu)
-                }
-                return menus
-            }
-            .take(1)
-            .bind(to: self.menuObservable)
-        
-    }
-    
-    func clearAllItemSlections() {
-        menuObservable
-            .observe(on: MainScheduler.instance)
-            .map{ menus in
-                menus.map{ m in
-                    Menu(id: m.id, name: m.name, price: m.price, count: 0)
-                }
-            }.take(1)
-            .subscribe(onNext: {
-                //self.menuObservable.onNext($0)
-                self.menuObservable.accept($0)
-            }).disposed(by: disposeBag)
-    }
-    
-    func changeCount(item: Menu, increase: Int) {
-        menuObservable
-            .observe(on: MainScheduler.instance)
-            .map{ menus in
-                menus.map { m in
-                    if m.id == item.id {
-                        return Menu(id: m.id, name: m.name, price: m.price, count: max(m.count + increase, 0))
-                        
-                    } else {
-                        return Menu(id: m.id, name: m.name, price: m.price, count: m.count)
-                    }
-                }
-            }
-            .take(1)
-            .subscribe(onNext: {
-                //self.menuObservable.onNext($0)
-                self.menuObservable.accept($0)
-            }).disposed(by: disposeBag)
-    }
-    
-    func onOrder() {
-        
-    }
-    
+//    
+//    var menuObservable = BehaviorRelay<[Menu]>(value: [])
+//    
+//    var disposeBag = DisposeBag()
+//    //var itemsCount: Int = 5
+//    //var totalPrice: PublishSubject<Int> = PublishSubject()
+//    
+//    lazy var itemCount = menuObservable.map {
+//        $0.map { $0.count }.reduce(0, +)
+//    }
+//    
+//    lazy var totalPrice = menuObservable.map {
+//        $0.map{ $0.price * $0.count }.reduce(0, +)
+//    }
+//    
+//    // subject
+//    // 서브젝트는 옵저버블 처럼 구독해서 값을 받을수도 있지만, 외부에서 값을 통제할 수 도 있다.
+//
+//    init() {
+////        var menus: [Menu] = [
+////            Menu(id: 0, name: "튀김1", price: 100, count: 0),
+////            Menu(id: 1, name: "튀김2", price: 100, count: 0),
+////            Menu(id: 2, name: "튀김3", price: 100, count: 0),
+////        ]
+//        
+//        _ = APIService.fetchAllMenusRx()
+//            .map { data -> [MenuItem] in
+//                struct Response: Decodable {
+//                    let menus: [MenuItem]
+//                }
+//                let response = try! JSONDecoder().decode(Response.self, from: data)
+//                return response.menus
+//            }.map{ menuItems -> [Menu] in
+//                var menus: [Menu] = []
+//                menuItems.enumerated().forEach { (index, item) in
+//                    let menu = Menu.fromMenuItems(id: index, item: item)
+//                    menus.append(menu)
+//                }
+//                return menus
+//            }
+//            .take(1)
+//            .bind(to: self.menuObservable)
+//        
+//    }
+//    
+//    func clearAllItemSlections() {
+//        menuObservable
+//            .observe(on: MainScheduler.instance)
+//            .map{ menus in
+//                menus.map{ m in
+//                    Menu(id: m.id, name: m.name, price: m.price, count: 0)
+//                }
+//            }.take(1)
+//            .subscribe(onNext: {
+//                //self.menuObservable.onNext($0)
+//                self.menuObservable.accept($0)
+//            }).disposed(by: disposeBag)
+//    }
+//    
+//    func changeCount(item: Menu, increase: Int) {
+//        menuObservable
+//            .observe(on: MainScheduler.instance)
+//            .map{ menus in
+//                menus.map { m in
+//                    if m.id == item.id {
+//                        return Menu(id: m.id, name: m.name, price: m.price, count: max(m.count + increase, 0))
+//                        
+//                    } else {
+//                        return Menu(id: m.id, name: m.name, price: m.price, count: m.count)
+//                    }
+//                }
+//            }
+//            .take(1)
+//            .subscribe(onNext: {
+//                //self.menuObservable.onNext($0)
+//                self.menuObservable.accept($0)
+//            }).disposed(by: disposeBag)
+//    }
+//    
+//    func onOrder() {
+//        
+//    }
+//    
 }
--- SwiftWithRxSwift/RxSwift4Hour/MVVM/Pages/MenuList/MenuViewController.swift
+++ SwiftWithRxSwift/RxSwift4Hour/MVVM/Pages/MenuList/MenuViewController.swift
@@ -11,65 +11,65 @@ import RxSwift
 import RxCocoa
 
 class MenuViewController: UIViewController {
-    // MARK: - Life Cycle
-    let cellID = "MenuItemTableViewCell"
-    let viewModel: MenuListViewModel = MenuListViewModel()
-    var disposeBag: DisposeBag = DisposeBag()
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
+//    // MARK: - Life Cycle
+//    let cellID = "MenuItemTableViewCell"
+//    let viewModel: MenuListViewModel = MenuListViewModel()
+//    var disposeBag: DisposeBag = DisposeBag()
+//
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//
+////        self.viewModel.itemCount
+////            .map{ "\($0)" }
+////            .subscribe(onNext: {
+////                self.itemCountLabel.text = $0
+////            }).disposed(by: disposeBag)
+//
+//        viewModel.menuObservable
+//            .bind(to: self.tableView.rx.items(cellIdentifier: self.cellID, cellType: MenuItemTableViewCell.self)) { index, item, cell in
+//                cell.title.text = item.name
+//                cell.price.text = "\(item.price)"
+//                cell.count.text = "\(item.count)"
+//
+//                // TableViewCell에 대한 액션 또한 해당 스트림에서 동작한다.
+//                cell.onChange = { [weak self] increase in
+//                    self?.viewModel.changeCount(item: item, increase: increase)
+//                }
+//
+//            }.disposed(by: disposeBag)
+//
+//
 //        self.viewModel.itemCount
-//            .map{ "\($0)" }
+//            .map{"\($0)"}
+//            .asDriver(onErrorJustReturn: "")
+//            .drive(itemCountLabel.rx.text)
+//            //.observe(on: MainScheduler.instance)
+//            //.bind(to: self.itemCountLabel.rx.text)
+//            .disposed(by: disposeBag)
+//
+//        self.viewModel.totalPrice
+//            .map{ $0.currencyKR() }
+//            .observe(on: MainScheduler.instance)
 //            .subscribe(onNext: {
-//                self.itemCountLabel.text = $0
+//                self.totalPrice.text = $0
 //            }).disposed(by: disposeBag)
-        
-        viewModel.menuObservable
-            .bind(to: self.tableView.rx.items(cellIdentifier: self.cellID, cellType: MenuItemTableViewCell.self)) { index, item, cell in
-                cell.title.text = item.name
-                cell.price.text = "\(item.price)"
-                cell.count.text = "\(item.count)"
-
-                // TableViewCell에 대한 액션 또한 해당 스트림에서 동작한다.
-                cell.onChange = { [weak self] increase in
-                    self?.viewModel.changeCount(item: item, increase: increase)
-                }
-                
-            }.disposed(by: disposeBag)
-        
-        
-        self.viewModel.itemCount
-            .map{"\($0)"}
-            .asDriver(onErrorJustReturn: "")
-            .drive(itemCountLabel.rx.text)
-            //.observe(on: MainScheduler.instance)
-            //.bind(to: self.itemCountLabel.rx.text)
-            .disposed(by: disposeBag)
-        
-        self.viewModel.totalPrice
-            .map{ $0.currencyKR() }
-            .observe(on: MainScheduler.instance)
-            .subscribe(onNext: {
-                self.totalPrice.text = $0
-            }).disposed(by: disposeBag)
-    }
-
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        let identifier = segue.identifier ?? ""
-        if identifier == "OrderViewController",
-            let orderVC = segue.destination as? OrderViewController {
-            // TODO: pass selected menus
-        }
-    }
-
-    func showAlert(_ title: String, _ message: String) {
-        let alertVC = UIAlertController(title: title, message: message, preferredStyle: .alert)
-        alertVC.addAction(UIAlertAction(title: "OK", style: .default))
-        present(alertVC, animated: true, completion: nil)
-    }
-
-    // MARK: - InterfaceBuilder Links
+//    }
+//
+//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+//        let identifier = segue.identifier ?? ""
+//        if identifier == "OrderViewController",
+//            let orderVC = segue.destination as? OrderViewController {
+//            // TODO: pass selected menus
+//        }
+//    }
+//
+//    func showAlert(_ title: String, _ message: String) {
+//        let alertVC = UIAlertController(title: title, message: message, preferredStyle: .alert)
+//        alertVC.addAction(UIAlertAction(title: "OK", style: .default))
+//        present(alertVC, animated: true, completion: nil)
+//    }
+//
+//    // MARK: - InterfaceBuilder Links
 
     @IBOutlet var activityIndicator: UIActivityIndicatorView!
     @IBOutlet var tableView: UITableView!
